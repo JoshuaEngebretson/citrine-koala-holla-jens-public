ba6dffa351d5b83947a52685eb012dbea5fb2e02
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-13 16:30:34 -0500

    Built out koalaRouter.delete

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -14,5 +14,35 @@ const koalaRouter = express.Router();
 
 
 // DELETE
+koalaRouter.delete('/:id', (req, res) => {
+    console.log(req.params);
+
+    let theIdToDelete = req.params.id;
+
+    // Will need to update the name of table for the
+    //  delete from SQL query
+    // Set up to sanitize the input when paired with
+    //  sqlValues.
+    let sqlText = `
+        DELETE from "koalas"
+            where "id"=$1;
+    `;
+
+    // Set up to pair with sqlText to sanitize input.
+    let sqlValues = [theIdToDelete];
+
+    pool.query(sqlText, sqlValues)
+        .then((dbRes) => {
+            // Send "Okay" to the client that declares this
+            //  delete was accepted/processed
+            res.sendStatus(200);
+        })
+        .catch((dbErr) => {
+            // Log that there was an issue with this function
+            console.log('delete /koalas/:id error:', dbErr);
+            // Send "Internal Server Error" status to client
+            res.sendStatus(500);
+        })
+})
 
 module.exports = koalaRouter;
